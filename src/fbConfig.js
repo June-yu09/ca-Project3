684a5cb15045176e276f327aa9ea3946af528fb9
June-yu09 <yuzatee@gmail.com>
2021-06-09 15:03:52 +0200

    project3

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -3,14 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyCkkSJmiEybGP_bI-JzwrLiBh0nmjbUR1A",
-    authDomain: "project3-73624.firebaseapp.com",
-    projectId: "project3-73624",
-    storageBucket: "project3-73624.appspot.com",
-    messagingSenderId: "884031688647",
-    appId: "1:884031688647:web:d8bf1044e97e5c861ad3e2",
-    
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
@@ -19,10 +17,3 @@ firebase.firestore();
 
 export default firebase;
 
-
-// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-// projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-// storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-// messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-// appId: process.env.REACT_APP_FIREBASE_APP_ID,
